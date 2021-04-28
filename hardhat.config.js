60f083f7f2b090f4195212f161670ce995f59f67
Matt Whitehead <matt.whitehead@colostate.edu>
2021-04-28 14:22:26 -0600

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,30 @@
+require("@nomiclabs/hardhat-waffle");
+
+const INFURA_PROJECT_ID = "project id";
+const RINKEBY_PRIVATE_KEY = "metamask private key";
+
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+
+module.exports = {
+  paths: {
+    sources: "./contract_ABI",
+    artifacts: "./artifacts"
+  },
+  solidity: "0.6.0",
+  networks: {
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${INFURA_PROJECT_ID}`,
+      accounts: [`0x${RINKEBY_PRIVATE_KEY}`]
+    }
+  }
+};
