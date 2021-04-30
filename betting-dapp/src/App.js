791bda208ef06808f1e1b6b80f9a738aaffa6f44
matt <mattmessina62@gmail.com>
2021-04-29 22:28:13 -0600

    UI now generates a button for each game on todays schedule

--- betting-dapp/src/App.js
+++ betting-dapp/src/App.js
@@ -1,26 +1,54 @@
 import React, { useEffect, useState } from 'react'; 
+import GameButton from "./GameButton";
 
+const generate_game_buttons = (todaysGames) => {
+  let gameButtons = [];
+  for(let i = 0; i < todaysGames.length; i++){
+    //TODO: pass in actual info on games
+    gameButtons.push(<GameButton label="foo" gameId="bar" />)
+  }
+  return gameButtons;
+}
 
 const App = () => {
   const [gotData, setGotData] = useState(false);
+  const [todaysGames, setTodaysGames] = useState([]);
+  const [gameButtons, setGameButtons] = useState([]);
 
+  //populate the matches for the day on startup
+  useEffect(()=>{
+    fetch("https://statsapi.web.nhl.com/api/v1/schedule")
+    .then(res => res.json())
+    .then(res => {
+      if(res.status >= 400){
+        throw new Error("Could not reach api endpoint. . .");
+      } else {
+        setTodaysGames(todaysGames.concat(res.dates));
+        setGotData(true);
+      }
+    })
+    .then(err => {
+      setGotData(false);
+    })
+  },[])
+
+  useEffect(() => {
+    if(todaysGames.length !== 0){
+      console.log(todaysGames[0].games.length);
+      setGameButtons(prevButtons => [...prevButtons, generate_game_buttons(todaysGames[0].games)])
+    }
+  }, [todaysGames])
+
+
+  
   return(
+    //TODO: generate buttons for each game acquired in the initial request
     <div>
-      <button onClick={() => {
-        //make API request and set gotData accordingly
-        fetch("https://www.thesportsdb.com/api/v1/json/1/searchfilename.php?e=English_Premier_League_2015-04-26_Arsenal_vs_Chelsea")
-        .then(res => {
-          if(res.status >= 400){
-            throw new Error("Could not reach api endpoint. . .");
-          } else {
-            setGotData(true);
-            console.log("Sent Request");
-          }
-        })
-        .then(err => {
-          setGotData(false);
-        })
-      }}>this is a button</button>
+      <ul className="list-group">
+        {gameButtons.length > 0 && gameButtons[0].map((button) => (
+          <li>{button}</li>
+        ))}
+      </ul>
     </div>
   )
 }
