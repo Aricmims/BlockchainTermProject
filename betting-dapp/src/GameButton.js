aa38305d226fac500680b6f436d7ad6fe82c7e52
matt <mattmessina62@gmail.com>
2021-05-01 10:17:40 -0600

    fixed up UI a little bit both visually and source code

--- betting-dapp/src/GameButton.js
+++ betting-dapp/src/GameButton.js
@@ -1,14 +1,27 @@
 import React, { useEffect, useState } from 'react'; 
+import Button from '@material-ui/core/Button';
+import { styled } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
+const GameButton = props => {
+    const MyButton = styled(Button)({
+        background: '#7289DA',
+        border: 0,
+        borderRadius: 3,
+        boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+        color: 'white',
+        height: 48,
+        padding: '30px 30px',
+    });
 
-const GameButton = (props) => {
-    
     return (
-        <button onClick={() => {
-            //TODO: 
-                // place a bet on the outcome of the specified game
-                // render a new page which shows updates on the match the user just betted on
-        }}> { props.home } vs. {props.away} </button>
+        <Grid container>
+            <Grid item>
+                <MyButton> {props.home} vs. {props.away} </MyButton>
+            </Grid>
+        </Grid>
+
+        
     )
 
 }
